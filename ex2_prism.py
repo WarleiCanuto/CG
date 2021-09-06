029e2ad7352be29f7a0aff0e6819406e7ce9bbc6
WarleiCanuto <48568968+WarleiCanuto@users.noreply.github.com>
2021-09-06 18:22:50 -0300

    Update ex2_prism.py

--- ex2_prism.py
+++ ex2_prism.py
@@ -5,7 +5,6 @@ import math
 
 cores = ( (0,0,1),(0.2,0.2,1),(0.4,0.4,1),(0.6,0.6,1),(0.8,0.8,1),(0.9,0.1,1),(0.8,0.2,1),(0.4,0.3,1) )
 quadro = 0
-#a = 0
 
 def desenha():
     global quadro
@@ -15,59 +14,36 @@ def desenha():
     glBegin(GL_TRIANGLE_FAN)
     glColor3fv(cores[0])
     raio = 0.5
-    
     glVertex3f(0,0,0)
     for i in range(0,6+7):
         a = (i/6) * 2 * math.pi
         x = raio * math.cos(a)
         y = raio * math.sin(a)
         glColor3fv(cores[(i+1)%len(cores)])
-        #glVertex3f(0,0,1)
- 
         glVertex3f(x,y, 0)
-        #glVertex3f(x,y, 1)
-        
-        #b = (i+1/6) * 2 * math.pi
-        #x2 = raio * math.cos(a)
-        #y2 = raio * math.sin(a)
-        #glVertex3f(x2,y2, 0)
-        #glVertex3f(x2,y2, 1)
-
     glEnd()
 
     glBegin(GL_TRIANGLE_FAN)
     glColor3fv(cores[0])
     raio = 0.5
-    
     glVertex3f(0,0,1)
     for i in range(0,6+7):
         a = (i/6) * 2 * math.pi
         x = raio * math.cos(a)
         y = raio * math.sin(a)
         glColor3fv(cores[(i+1)%len(cores)])
-        #glVertex3f(0,0,1)
- 
         glVertex3f(x,y, 1)
-        #glVertex3f(x,y, 1)
-        
-        #b = (i+1/6) * 2 * math.pi
-        #x2 = raio * math.cos(a)
-        #y2 = raio * math.sin(a)
-        #glVertex3f(x2,y2, 0)
-        #glVertex3f(x2,y2, 1)
     glEnd()
 
     glBegin(GL_QUAD_STRIP)
     glColor3fv(cores[0])
     raio = 0.5
-    
-    #glVertex3f(0,0,1)
+ 
     for i in range(0,6+7):
         a = (i/6) * 2 * math.pi
         x = raio * math.cos(a)
         y = raio * math.sin(a)
         glColor3fv(cores[(i+1)%len(cores)])
-        #glVertex3f(0,0,1)
  
         glVertex3f(x,y, 0)
         glVertex3f(x,y, 1)
