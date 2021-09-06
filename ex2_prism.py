7906072b088e7f6fe268b47918f8263b6d6f19b0
WarleiCanuto <48568968+WarleiCanuto@users.noreply.github.com>
2021-09-06 18:20:36 -0300

    Update ex2_prism.py

--- ex2_prism.py
+++ ex2_prism.py
@@ -3,7 +3,7 @@ from OpenGL.GLU import *
 from OpenGL.GL import *
 import math
 
-cores = ( (1,0,0),(1,1,0),(0,1,0),(0,1,1),(0,0,1),(1,0,1),(0.5,1,1),(1,0,0.5) )
+cores = ( (0,0,1),(0.2,0.2,1),(0.4,0.4,1),(0.6,0.6,1),(0.8,0.8,1),(0.9,0.1,1),(0.8,0.2,1),(0.4,0.3,1) )
 quadro = 0
 #a = 0
 
@@ -14,25 +14,71 @@ def desenha():
     glRotatef(quadro,1,1,0)
     glBegin(GL_TRIANGLE_FAN)
     glColor3fv(cores[0])
-    glVertex3f(0,0,0)
     raio = 0.5
+    
+    glVertex3f(0,0,0)
     for i in range(0,6+7):
         a = (i/6) * 2 * math.pi
         x = raio * math.cos(a)
         y = raio * math.sin(a)
         glColor3fv(cores[(i+1)%len(cores)])
+        #glVertex3f(0,0,1)
+ 
+        glVertex3f(x,y, 0)
+        #glVertex3f(x,y, 1)
+        
+        #b = (i+1/6) * 2 * math.pi
+        #x2 = raio * math.cos(a)
+        #y2 = raio * math.sin(a)
+        #glVertex3f(x2,y2, 0)
+        #glVertex3f(x2,y2, 1)
+
+    glEnd()
+
+    glBegin(GL_TRIANGLE_FAN)
+    glColor3fv(cores[0])
+    raio = 0.5
+    
+    glVertex3f(0,0,1)
+    for i in range(0,6+7):
+        a = (i/6) * 2 * math.pi
+        x = raio * math.cos(a)
+        y = raio * math.sin(a)
+        glColor3fv(cores[(i+1)%len(cores)])
+        #glVertex3f(0,0,1)
+ 
+        glVertex3f(x,y, 1)
+        #glVertex3f(x,y, 1)
+        
+        #b = (i+1/6) * 2 * math.pi
+        #x2 = raio * math.cos(a)
+        #y2 = raio * math.sin(a)
+        #glVertex3f(x2,y2, 0)
+        #glVertex3f(x2,y2, 1)
+    glEnd()
+
+    glBegin(GL_QUAD_STRIP)
+    glColor3fv(cores[0])
+    raio = 0.5
+    
+    #glVertex3f(0,0,1)
+    for i in range(0,6+7):
+        a = (i/6) * 2 * math.pi
+        x = raio * math.cos(a)
+        y = raio * math.sin(a)
+        glColor3fv(cores[(i+1)%len(cores)])
+        #glVertex3f(0,0,1)
+ 
         glVertex3f(x,y, 0)
         glVertex3f(x,y, 1)
-
-    '''
-    for i in range(0,6+7):
-        a = (i/6) * 2 * math.pi
-        x = raio * math.cos(a)
-        y = raio * math.sin(a)
-        glColor3fv(cores[(i+1)%len(cores)])
-        glVertex3f(x,y, 1)
-    '''
+        
+        b = (i+1/6) * 2 * math.pi
+        x2 = raio * math.cos(a)
+        y2 = raio * math.sin(a)
+        glVertex3f(x2,y2, 0)
+        glVertex3f(x2,y2, 1)
     glEnd()
+
     glPopMatrix()
     glutSwapBuffers()
     quadro += 5
